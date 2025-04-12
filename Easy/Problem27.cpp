eac3b8fbb58a427dc60c1aef8b7e88c82d93bc76
Yazan <isryman@protonmail.com>
2025-04-12 20:12:07 +0300

    Added problem 27

---
+++ Easy/Problem27.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+int removeElement(vector<int>& nums, int val) {
+    int k = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+}
