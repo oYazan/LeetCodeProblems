132d1fc56995e9fd49c86ecdcd2cd8731d5af5e9
Yazan <isryman@protonmail.com>
2025-04-12 19:20:40 +0300

    Added problem 20 solution

---
+++ Problem20.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> openChars;
+    for (int i = 0; i < s.length(); i++) {
+        char c = s[i];
+
+        if (c == '(' || c == '[' || c == '{') {
+            openChars.push(c);
+        } else {
+            if (openChars.empty()) {
+                return false;
+            }
+            if ((c == ')' && openChars.top() == '(') ||
+                (c == ']' && openChars.top() == '[') ||
+                (c == '}' && openChars.top() == '{')) {
+                openChars.pop();
+                } else {
+                    return false;
+                }
+        }
+    }
+    return openChars.empty();
+}
+
+int main() {
+    string input = "()[]{}";
+    cout << "Valid: " << (isValid(input) ? "true" : "false") << endl;
+    return 0;
+}
