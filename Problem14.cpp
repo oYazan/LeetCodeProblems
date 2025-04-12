0d846c0830984735087e48dc406d13443197414b
Yazan <isryman@protonmail.com>
2025-04-12 18:34:09 +0300

    Added Problem 14 Solution

---
+++ Problem14.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) return "";
+    string prefix = "";
+    for (int j = 0; j < strs[0].size(); j++) {
+        char current = strs[0][j];
+        for (int i = 0; i < strs.size(); i++) {
+            if (j >= strs[i].size() || strs[i][j] != current) {
+                return prefix;
+            }
+        }
+        prefix += current;
+    }
+    return prefix;
+}
+
+int main() {
+    vector<string> myVec = {"flower", "flow", "flight"};
+    cout << "Most common: " << longestCommonPrefix(myVec);
+    return 0;
+}
